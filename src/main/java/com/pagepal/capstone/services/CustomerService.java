ddfd1c2d1b6356971e8b823d8d913d5abe306676
blvm159263 <builevanminh0809@gmail.com>
2024-01-04 17:47:20 +0700

    Done get Customer Detail

--- src/main/java/com/pagepal/capstone/services/CustomerService.java
+++ src/main/java/com/pagepal/capstone/services/CustomerService.java
@@ -4,7 +4,10 @@ import com.pagepal.capstone.dtos.customer.CustomerDto;
 import com.pagepal.capstone.dtos.reader.ReaderDto;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CustomerService {
     List<CustomerDto> getCustomersActive();
+
+    CustomerDto getCustomerById(UUID id);
 }
