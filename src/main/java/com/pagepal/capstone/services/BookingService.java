b74b691ac22b0c8a223bd43d1c12070a9dfe9d0b
blvm159263 <builevanminh0809@gmail.com>
2024-03-03 09:12:21 +0700

    Fixed

--- src/main/java/com/pagepal/capstone/services/BookingService.java
+++ src/main/java/com/pagepal/capstone/services/BookingService.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface BookingService {
-    List<BookingDto> getListBookingByReader(UUID readerId);
+    ListBookingDto getListBookingByReader(UUID readerId, QueryDto queryDto);
 
     ListBookingDto getListBookingByCustomer(UUID cusId, QueryDto queryDto);
 
