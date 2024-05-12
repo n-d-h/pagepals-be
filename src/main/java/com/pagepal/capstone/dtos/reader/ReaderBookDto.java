9d690be0d3b8f9f24433b85d1adf8e53658b0c6f
n-d-h <duchoang4195@gmail.com>
2024-05-12 22:53:37 +0700

    fix

--- src/main/java/com/pagepal/capstone/dtos/reader/ReaderBookDto.java
+++ src/main/java/com/pagepal/capstone/dtos/reader/ReaderBookDto.java
@@ -16,7 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 public class ReaderBookDto {
     private BookDto book;
-    //    private List<ServiceDto> services = new ArrayList<>();
+    private List<ServiceDto> services = new ArrayList<>();
     private Integer servicesCount;
     private Integer ratingAverage;
     private Integer totalReview;
