7e028b1cb334d74b34834e20df4eae7efdc7bfe0
n-d-h <duchoang4195@gmail.com>
2024-05-14 18:32:59 +0700

    commit

--- src/main/java/com/pagepal/capstone/services/impl/EventServiceImpl.java
+++ src/main/java/com/pagepal/capstone/services/impl/EventServiceImpl.java
@@ -111,6 +111,10 @@ public class EventServiceImpl implements EventService {
                 throw new ValidationException("Advertise date is required for featured event");
             }
 
+            if (dto.getLimitCustomer() < 0) {
+                throw new ValidationException("Limit customer must be greater than 0");
+            }
+
             Meeting meeting = zoomService.createMeeting(
                     reader,
                     seminar.getTitle(),
@@ -200,15 +204,15 @@ public class EventServiceImpl implements EventService {
                 throw new ValidationException("Event not found");
             }
 
-            var eventBookingsNumber = event.getLimitCustomer() - event.getActiveSlot();
-            var newActiveSlot = eventDto.getLimitCustomer() - eventBookingsNumber;
+
+            if (eventDto.getLimitCustomer() < 0) {
+                throw new ValidationException("Cannot set limit customer less than bookings number");
+            }
 
             event.setLimitCustomer(eventDto.getLimitCustomer());
-            event.setActiveSlot(newActiveSlot);
-            if (eventBookingsNumber == 0) {
-                event.setStartAt(dateFormat.parse(eventDto.getStartAt()));
-                event.setPrice(eventDto.getPrice());
-            }
+            event.setActiveSlot(eventDto.getLimitCustomer());
+            event.setStartAt(dateFormat.parse(eventDto.getStartAt()));
+            event.setPrice(eventDto.getPrice());
 
             eventRepository.save(event);
             return convertToDto(event);
