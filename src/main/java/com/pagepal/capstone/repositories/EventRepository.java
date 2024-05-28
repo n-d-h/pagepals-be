7a0876a7c21d06c87e0e902bda93493f0b03a5da
n-d-h <duchoang4195@gmail.com>
2024-05-28 11:35:07 +0700

    commit

--- src/main/java/com/pagepal/capstone/repositories/EventRepository.java
+++ src/main/java/com/pagepal/capstone/repositories/EventRepository.java
@@ -175,10 +175,8 @@ public interface EventRepository extends JpaRepository<Event, UUID> {
             FROM Event e
             WHERE e.state = :state
             AND e.startAt > :currentTime
-            AND e.seminar.status = :seminarStatus
-            AND e.seminar.state = :seminarState
             ORDER BY e.startAt ASC, e.seminar.reader.rating DESC
             """)
-    List<Event> findTop10ActiveEventsOrderByStartAtAscAndReaderRatingDesc(EventStateEnum state, Date currentTime, Status seminarStatus, SeminarStatus seminarState);
+    List<Event> findTop10ActiveEventsOrderByStartAtAscAndReaderRatingDesc(EventStateEnum state, Date currentTime);
 
 }
